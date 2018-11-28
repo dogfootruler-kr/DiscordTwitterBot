5824943c73311fd49ca61c9f188ec8ae2291e3ee
Patrice Lynde <patrice.lynde@epitech.eu>
2018-11-28 17:04:38 +0100

    WIP

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+require('dotenv').config()
 var Discord = require('discord.io');
 var logger = require('winston');
 var Twitter = require('twitter');
 
-var client = new Twitter({
+
+/*var client = new Twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token_key: process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
+});*/
 
-var accountList = ['plynde'];
+var accountList = [{
+    channelID: '244864136188526594',
+    twitterAccount: 'plynde'
+}];
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -31,10 +36,12 @@ bot.on('ready', function(evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-client.stream('statuses/filter', { follow: accountList.join(',') }, function(stream) {
+/*client.stream('statuses/filter', { follow: accountList.map(a => a.twitterAccount).join(',') }, function(stream) {
     stream.on('data', function(event) {
+        // base on twitter account send it to the right channel
+        var result = accountList.filter(obj => obj.twitterAccount === event.username); // to change
         bot.sendMessage({
-            to: channelID,
+            to: result.channelID,
             message: event.text
         });
         
@@ -44,18 +51,23 @@ client.stream('statuses/filter', { follow: accountList.join(',') }, function(str
     stream.on('error', function(error) {
         throw error;
     });
-});
+});*/
 
 bot.on('message', function(user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
 
+        console.log(user, userID, channelID, message, evt);
+
         //args = args.splice(1);
         switch (cmd) {
             // !follow
             case 'follow':
-                accountList.push(args[1]);
+                accountList.push({
+                    channelID: evt.d.channel_id,
+                    twitterAccount: args[1]
+                });
                 bot.sendMessage({
                     to: channelID,
                     message: `J'ai ajoute ${args[1]} dans la liste des comptes a suivre.`
@@ -65,13 +77,12 @@ bot.on('message', function(user, userID, channelID, message, evt) {
             case 'list':
                 bot.sendMessage({
                     to: channelID,
-                    message: `Voici la liste des comptes: ${accountList.join(', ')}.`
+                    message: `Voici la liste des comptes: ${accountList.map(a => a.twitterAccount).join(', ')}.`
                 });
                 break;
             // !remove
             case 'remove':
-                var index = accountList.indexOf(args[1]);
-                if (index !== -1) accountList.splice(index, 1);
+                accountList = accountList.filter(obj => obj.twitterAccount !== args[1]);
                 bot.sendMessage({
                     to: channelID,
                     message: `J'ai enleve ${args[1]} de la liste des comptes a suivre.`
